d4078154dcc3c1f856bc1cd54f9706bf4c693161
KatieS <30733981+Abareyellow@users.noreply.github.com>
2018-12-26 10:14:12 -0500

    Prototype

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+require './app/models/model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :views, 'app/views'
+  end
+
+  post '/results' do
+    answers = params.values
+    @total = 0
+    answers.each do |answer|
+      @total += answer.to_i
+    end
+    puts @total
+
+    @combo = fortune_generator(@total)
+    if @combo == "bear"
+      erb :bear
+    elsif @combo == "newton"
+      erb :newton
+    elsif @combo == "muppet"
+      erb :muppet
+    else:
+      erb :fairyGodMother
+    end
+  end
+end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,13 @@
 def fortune_generator(user_input):
 
   if user_input == 15:
-    return ""
+    return "bear"
+  elsif user_input == 20:
+    return "newton"
+  elsif user_input == 25:
+    return "muppet"
+  else:
+    return "fairyGodMother"
+  end
+  
+end
